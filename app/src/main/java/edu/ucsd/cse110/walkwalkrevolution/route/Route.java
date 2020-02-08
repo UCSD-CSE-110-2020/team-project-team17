eeb5d6ac5ed95900a1bafd06899054d8c4c9722c
brain <librian415@gmail.com>
2020-02-07 22:09:59 -0800

    Added Route Class and SharedPreference

---
+++ app/src/main/java/edu/ucsd/cse110/walkwalkrevolution/route/Route.java
@@ -0,0 +1,11 @@
+package edu.ucsd.cse110.walkwalkrevolution.route;
+
+public class Route {
+
+    long id;
+
+    public Route(long id){
+        this.id = id;
+    }
+
+}
---
+++ app/src/main/java/edu/ucsd/cse110/walkwalkrevolution/route/RoutePersistence.java
@@ -0,0 +1,43 @@
+package edu.ucsd.cse110.walkwalkrevolution.route;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import java.util.Map;
+
+import edu.ucsd.cse110.walkwalkrevolution.WalkWalkRevolution;
+
+import static android.content.Context.MODE_PRIVATE;
+
+public class RoutePersistence {
+
+    private static final String SP_ROUTE = "ROUTE";
+
+    private static void addRoute(Route route){
+        Context context = WalkWalkRevolution.getContext();
+        SharedPreferences sp = context.getSharedPreferences(SP_ROUTE, MODE_PRIVATE);
+        SharedPreferences.Editor editor = sp.edit();
+
+        //TODO: REPLACE VALUE WITH TITLE OF ROUTE -> USED AS NAME FOR SEPERATE SHARED PREF
+        editor.putString(Long.toString(route.id), Long.toString(route.id));
+
+        editor.apply();
+    }
+
+    private static Route getRoute(long routeId){
+        Context context = WalkWalkRevolution.getContext();
+        SharedPreferences sp = context.getSharedPreferences(SP_ROUTE, MODE_PRIVATE);
+
+        String identifier = sp.getString(Long.toString(routeId), "");
+
+        return new Route(Long.parseLong(identifier));
+    }
+
+    private static Map<String, ?> getAllRoutes() {
+        Context context = WalkWalkRevolution.getContext();
+        SharedPreferences sp = context.getSharedPreferences(SP_ROUTE, MODE_PRIVATE);
+        Map<String, ?> Routes = sp.getAll();
+        return Routes;
+    }
+
+}
