19c86810143461cc02ea51d4bbe0f23bae601db6
brain <librian415@gmail.com>
2020-02-08 10:16:41 -0800

    Added Route and Route Persistence Class

--- app/src/main/java/edu/ucsd/cse110/walkwalkrevolution/WalkWalkRevolution.java
+++ app/src/main/java/edu/ucsd/cse110/walkwalkrevolution/WalkWalkRevolution.java
@@ -12,10 +12,13 @@ public class WalkWalkRevolution extends Application {
 
     public static String fitnessServiceKey = "GOOGLE_FIT";
 
+    private static Context context;
+
     @Override
     public void onCreate() {
         super.onCreate();
         setupGoogleFitnessApi();
+        WalkWalkRevolution.context = getApplicationContext();
     }
 
     private void setupGoogleFitnessApi() {
@@ -29,6 +32,10 @@ public class WalkWalkRevolution extends Application {
 
     public void setFitnessServiceKey(String fitnessServiceKey) {
         this.fitnessServiceKey = fitnessServiceKey;
+    }
+
+    public static Context getContext() {
+        return context;
     }
 
 }
