7246ed276e3eb64d5cadb9bd8de296ecae8485df
justin <justinram30@gmail.com>
2020-02-08 12:32:30 -0800

    Edited Tag constants and implemented Unit test for Route.java

--- app/src/main/java/edu/ucsd/cse110/walkwalkrevolution/route/Tag.java
+++ app/src/main/java/edu/ucsd/cse110/walkwalkrevolution/route/Tag.java
@@ -7,19 +7,19 @@ public class Tag {
     public static final int OUT_BACK = 2;
 
     // Route hill types: (1) hilly, (2) flat
-    public static final int HILL = 1;
-    public static final int FLAT = 2;
+    public static final int HILL = 3;
+    public static final int FLAT = 4;
 
     // Route area types: (1) street, (2) trail
-    public static final int STREET = 1;
-    public static final int TRAIL = 2;
+    public static final int STREET = 5;
+    public static final int TRAIL = 6;
 
     // Route surface types (1) even, (2) uneven
-    public static final int EVEN = 1;
-    public static final int UNEVEN = 2;
+    public static final int EVEN = 7;
+    public static final int UNEVEN = 8;
 
     // Route difficulty (1) easy, (2) moderate, (3) difficult
-    public static final int EASY = 1;
-    public static final int MODERATE = 2;
-    public static final int DIFFICULT = 3;
+    public static final int EASY = 10;
+    public static final int MODERATE = 11;
+    public static final int DIFFICULT = 12;
 }
