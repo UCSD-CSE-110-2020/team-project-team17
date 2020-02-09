9855b90d7163f7803141514ae992db8a14276c44
brain <librian415@gmail.com>
2020-02-08 20:44:11 -0800

    Added Routes Tests for multiple routes

--- app/src/androidTest/java/edu/ucsd/cse110/walkwalkrevolution/RoutesTest.java
+++ app/src/androidTest/java/edu/ucsd/cse110/walkwalkrevolution/RoutesTest.java
@@ -9,8 +9,11 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
 
 import edu.ucsd.cse110.walkwalkrevolution.activity.Activity;
+import edu.ucsd.cse110.walkwalkrevolution.activity.ActivityUtils;
 import edu.ucsd.cse110.walkwalkrevolution.activity.Walk;
 import edu.ucsd.cse110.walkwalkrevolution.route.Route;
 import edu.ucsd.cse110.walkwalkrevolution.route.persistence.MockRouteDao;
@@ -49,6 +52,46 @@ public class RoutesTest {
                 assertEquals(1, currentRecyclerView.getAdapter().getItemCount());
                 RoutesAdapter.ViewHolder holder = (RoutesAdapter.ViewHolder) currentRecyclerView.findViewHolderForLayoutPosition(0);
                 assertEquals("Route 1", holder.routeTitle.getText().toString());
+                assertEquals("0", holder.steps.getText().toString());
+                assertEquals("0", holder.miles.getText().toString());
+                assertEquals("0", holder.duration.getText().toString());
+                assertEquals("01/01", holder.date.getText().toString());
+            });
+        }
+    }
+
+    @Test
+    public void hasMultipleStoredRoutes(){
+        Activity walk = new Walk();
+        walk.setDate(LocalDateTime.of(2020, 1, 1, 0, 0));
+        WalkWalkRevolution.getRouteDao().addRoute(new Route(1, "z", walk));
+
+        LocalDateTime time = LocalDateTime.of(2020, 01, 01, 0, 0);
+        Map<String, String> data = new HashMap<String, String>() {{
+            put(Walk.STEP_COUNT, "500");
+            put(Walk.MILES, "0.25");
+            put(Walk.DURATION, "5:00");
+            put(Activity.DATE, ActivityUtils.timeToString(time));
+        }};
+        WalkWalkRevolution.getRouteDao().addRoute(new Route(2, "a", new Walk(data)));
+
+
+        try(ActivityScenario<RoutesActivity> scenario = ActivityScenario.launch(RoutesActivity.class)){
+            scenario.onActivity(activity -> {
+                RecyclerView currentRecyclerView = ((RecyclerView) activity.findViewById(R.id.routes));
+                assertEquals(2, currentRecyclerView.getAdapter().getItemCount());
+
+                //Note: Also, tests alphabetical ordering
+
+                RoutesAdapter.ViewHolder holder = (RoutesAdapter.ViewHolder) currentRecyclerView.findViewHolderForLayoutPosition(0);
+                assertEquals("a", holder.routeTitle.getText().toString());
+                assertEquals("500", holder.steps.getText().toString());
+                assertEquals("0.25", holder.miles.getText().toString());
+                assertEquals("5:00", holder.duration.getText().toString());
+                assertEquals("01/01", holder.date.getText().toString());
+
+                holder = (RoutesAdapter.ViewHolder) currentRecyclerView.findViewHolderForLayoutPosition(1);
+                assertEquals("z", holder.routeTitle.getText().toString());
                 assertEquals("0", holder.steps.getText().toString());
                 assertEquals("0", holder.miles.getText().toString());
                 assertEquals("0", holder.duration.getText().toString());
