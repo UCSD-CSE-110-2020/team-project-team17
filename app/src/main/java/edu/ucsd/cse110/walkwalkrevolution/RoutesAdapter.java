a31331fd34c06d5559d846d547212a50199d9332
Jackson Tuxhorn <jtuxhorn@ucsd.edu>
2020-02-14 21:34:18 -0800

    Basic clickable routes list functionality

--- app/src/main/java/edu/ucsd/cse110/walkwalkrevolution/RoutesAdapter.java
+++ app/src/main/java/edu/ucsd/cse110/walkwalkrevolution/RoutesAdapter.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -54,6 +55,16 @@ public class RoutesAdapter extends
 
         // Return a new holder instance
         ViewHolder viewHolder = new ViewHolder(contactView);
+
+
+        contactView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int index = RoutesActivity.recyclerView.getChildAdapterPosition(view);
+                Route item = routes.get(index);
+                Toast.makeText(context, item.getTitle(), Toast.LENGTH_LONG).show();
+            }
+        });
         return viewHolder;
     }
 
