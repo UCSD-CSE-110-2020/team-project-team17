60616e2550796e2689f6f4b47dac93130dd096d2
Jackson Tuxhorn <jtuxhorn@ucsd.edu>
2020-03-03 15:23:40 -0800

    Updated test again

--- app/src/androidTest/java/edu/ucsd/cse110/walkwalkrevolution/TeamDetailsActivityTest.java
+++ app/src/androidTest/java/edu/ucsd/cse110/walkwalkrevolution/TeamDetailsActivityTest.java
@@ -1,8 +1,5 @@
 package edu.ucsd.cse110.walkwalkrevolution;
 
-import android.content.Intent;
-import android.os.Bundle;
-
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.test.core.app.ActivityScenario;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
@@ -10,46 +7,19 @@ import androidx.test.ext.junit.runners.AndroidJUnit4;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mockito;
 
-import java.time.LocalDateTime;
-import java.util.HashMap;
-import java.util.Map;
-
-import edu.ucsd.cse110.walkwalkrevolution.activity.Activity;
-import edu.ucsd.cse110.walkwalkrevolution.activity.ActivityUtils;
-import edu.ucsd.cse110.walkwalkrevolution.activity.Walk;
-import edu.ucsd.cse110.walkwalkrevolution.route.Route;
 import edu.ucsd.cse110.walkwalkrevolution.route.RouteRecycleView.RoutesAdapter;
 import edu.ucsd.cse110.walkwalkrevolution.route.persistence.MockRouteDao;
-import edu.ucsd.cse110.walkwalkrevolution.route.persistence.RouteService;
-import edu.ucsd.cse110.walkwalkrevolution.route.persistence.RouteServiceFactory;
-import edu.ucsd.cse110.walkwalkrevolution.team.Team;
-import edu.ucsd.cse110.walkwalkrevolution.user.persistence.UserService;
-import edu.ucsd.cse110.walkwalkrevolution.user.persistence.UserServiceFactory;
 
 import static junit.framework.TestCase.assertEquals;
-import static org.mockito.Mockito.when;
 
 @RunWith(AndroidJUnit4.class)
 public class TeamDetailsActivityTest {
-
-    TeamDetailsActivity teamDetailsActivity;
-    RecyclerView rvUsers;
-
-    RouteService routeService;
-    RouteServiceFactory routeServiceFactory;
-    UserService userService;
-    UserServiceFactory userServiceFactory;
-
-    Team team;
 
     @Before
     public void setUp() {
         TeamDetailsActivity.testMode = true; // Bigbrain
         WalkWalkRevolution.setRouteDao(new MockRouteDao());
-
-        team = Mockito.mock(Team.class);
     }
 
 
