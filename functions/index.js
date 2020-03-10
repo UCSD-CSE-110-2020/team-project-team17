9202d59335e5ab0ed87aa432c5a325c37855b5e5
brain <librian415@gmail.com>
2020-03-10 16:27:36 -0700

    Updated team invite notifications

--- functions/index.js
+++ functions/index.js
@@ -3,23 +3,60 @@ const admin = require('firebase-admin');
 admin.initializeApp();
 
 exports.addDefaultTeam = functions.firestore
-    .document('user/{userId}')
-    .onCreate((snap, context) => {
-      if (snap) {
-        return snap.ref.update({
-                    team: snap.data()['email']
-                })
-      }
+           .document('user/{userId}')
+           .onCreate((snap, context) => {
+             if (snap) {
+               return snap.ref.update({
+                           team: snap.data()['email']
+                       })
+             }
 
-      return "snap was null or empty";
-    });
+             return "snap was null or empty";
+           });
+
+exports.sendTeamInvite = functions.firestore
+          .document('invitation/{invitationId}')
+          .onCreate((snap, context) => {
+
+          const document = snap.exists ? snap.data() : null;
+
+          if (document) {
+             var t = document.to.replace('@', '.')
+
+             var message = {
+               notification: {
+                 title: 'New Invitation',
+                 body: document.sender + ' has sent you an invite.'
+               },
+               topic: t
+             };
+
+             console.log("Sending message " + message)
+             return admin.messaging().send(message)
+               .then((response) => {
+                 // Response is a message ID string.
+                 console.log('Successfully sent message:', response);
+                 return response;
+               })
+               .catch((error) => {
+                 console.log('Error sending message:', error);
+                 return error;
+               });
+           }
+
+           console.log("No document found")
+           return "document was null or empty";
+         });
 
 exports.sendTeamNotifications = functions.firestore
    .document('user/{userId}')
    .onUpdate((snap, context) => {
      // Get an object with the current document value.
      // If the document does not exist, it has been deleted.
-     const document = snap.exists ? snap.data() : null;
+     const document = snap.after.exists ? snap.after.data() : null;
+
+     console.log("Checking if document exists")
+
 
      if (document) {
 
@@ -33,6 +70,7 @@ exports.sendTeamNotifications = functions.firestore
          topic: t
        };
 
+       console.log("Sending message " + message)
        return admin.messaging().send(message)
          .then((response) => {
            // Response is a message ID string.
@@ -45,5 +83,7 @@ exports.sendTeamNotifications = functions.firestore
          });
      }
 
+     console.log("No document found")
      return "document was null or empty";
    });
+
